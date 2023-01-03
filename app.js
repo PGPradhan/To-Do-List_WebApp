f644209164d33453ee3589b01692e632ca0b5ab1
PGPradhan <pradhan.prathamesh10@gmail.com>
2023-01-03 23:32:00 +0530

    Commit 2

--- app.js
+++ app.js
@@ -3,21 +3,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 app.set("view-engine", "ejs");
-app.get("/",(req,res)=>{
+app.get("/",function(req,res){
 
   var today = new Date();
+  var currentDay = today.getDate()
   var day = "";
-  if (today.getDate()==6 || today.getDate()==0){
+  if (currentDay==6 || currentDay==0){
     day="Weekend";
   }else{
     day="Weekday";
 
   }
-  res.render("list", {anyDay:day});
+  res.render("list.ejs", {anyDay:day});
 });
 
-app.listen(3000,function(){
-  console.log("Server is running on port 3000");
+app.listen(8000,function(){
+  console.log("Server is running on port 8000");
 });
