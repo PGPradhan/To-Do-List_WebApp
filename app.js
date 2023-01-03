db007009c7b0f53c7287bf578ee796a49d271086
PGPradhan <pradhan.prathamesh10@gmail.com>
2023-01-03 23:49:53 +0530

    First branch Commit

--- app.js
+++ app.js
@@ -6,20 +6,43 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view-engine", "ejs");
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
 
   var today = new Date();
   var currentDay = today.getDate()
   var day = "";
-  if (currentDay==6 || currentDay==0){
-    day="Weekend";
-  }else{
-    day="Weekday";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday"
+      break;
+    case 1:
+      day = "Monday"
+      break;
+    case 2:
+      day = "Tuesday"
+      break;
+    case 3:
+      day = "Wednessday"
+      break;
+    case 4:
+      day = "Thursday"
+      break;
+    case 5:
+      day = "Friday"
+      break;
+    case 6:
+      day = "Saturday"
+      break;
+
+    default:
+      day="Not valid Date"
 
   }
-  res.render("list.ejs", {anyDay:day});
+  res.render("list.ejs", {
+    anyDay: day
+  });
 });
 
-app.listen(8000,function(){
+app.listen(8000, function() {
   console.log("Server is running on port 8000");
 });
