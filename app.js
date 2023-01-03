590241bdd78aa5771280da266f8ed86368762323
PGPradhan <pradhan.prathamesh10@gmail.com>
2023-01-04 02:03:46 +0530

    Passing data from webpage to Server

--- app.js
+++ app.js
@@ -5,43 +5,31 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+
+const items = ["Buy food","Cook food","Eat food"];
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view-engine", "ejs");
 app.get("/", function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDate()
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday"
-      break;
-    case 1:
-      day = "Monday"
-      break;
-    case 2:
-      day = "Tuesday"
-      break;
-    case 3:
-      day = "Wednessday"
-      break;
-    case 4:
-      day = "Thursday"
-      break;
-    case 5:
-      day = "Friday"
-      break;
-    case 6:
-      day = "Saturday"
-      break;
+  var options = {
+    weekday: "long",
+    day:"numeric",
+    month:"long"
+  };
+  var day = today.toLocaleDateString("en-US",options);
 
-    default:
-      day="Not valid Date"
-
-  }
   res.render("list.ejs", {
-    anyDay: day
+    anyDay: day,newListItems: items
   });
 });
+
+app.post("/",function(req,res){
+   var item = req.body.newItem;
+   items.push(item);
+  res.redirect("/");
+})
 
 app.listen(8000, function() {
   console.log("Server is running on port 8000");
