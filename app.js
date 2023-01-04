495ba08e9b051b55045be2bee9feae54e28acd46
PGPradhan <pradhan.prathamesh10@gmail.com>
2023-01-04 23:33:47 +0530

    Home and Work two separate pages

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const items = ["Buy food","Cook food","Eat food"];
-app.use(bodyParser.urlencoded({extended:true}));
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItem = [];
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 
 app.set("view-engine", "ejs");
 app.get("/", function(req, res) {
@@ -15,21 +20,38 @@ app.get("/", function(req, res) {
   var today = new Date();
   var options = {
     weekday: "long",
-    day:"numeric",
-    month:"long"
+    day: "numeric",
+    month: "long"
   };
-  var day = today.toLocaleDateString("en-US",options);
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list.ejs", {
-    anyDay: day,newListItems: items
+    ListTitle: day,
+    newListItems: items
   });
 });
 
-app.post("/",function(req,res){
-   var item = req.body.newItem;
-   items.push(item);
-  res.redirect("/");
-})
+app.post("/", function(req, res) {
+  //var item = req.body.newItem;
+  let item = req.body.newItem;
+  if (req.body.list == "Work") {
+    workItem.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list.ejs", {
+    ListTitle: "Work List",
+    newListItems: workItem
+  });
+});
+
+
 
 app.listen(8000, function() {
   console.log("Server is running on port 8000");
